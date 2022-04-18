40cfb04ae6859b25e53bc2ef0bd3e6643ebed349
Ricardo <ricardomg92@hotmail.com>
2022-04-18 18:05:49 +0200

    Call insert method with a random number between 1 and 100 and print the Tree with the inserted value

--- lib/main.rb
+++ lib/main.rb
@@ -3,7 +3,12 @@
 require_relative 'tree'
 
 tree = Tree.new
-tree.build_tree((Array.new(rand(1..15)) { rand(1..100) }))
+tree.build_tree(Array.new(rand(1..15)) { rand(1..100) })
+puts ''
+tree.pretty_print
+puts ''
+
+tree.insert(rand(1..100))
 puts ''
 tree.pretty_print
 puts ''
