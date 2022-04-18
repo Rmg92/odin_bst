9f7b8f7cba872f7ff2c27e0dc7bc903db060c933
Ricardo <ricardomg92@hotmail.com>
2022-04-18 09:59:21 +0200

    Add build_tree method that sort and removes repeated items from an inputted array and build a balanced tree with it

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,8 +1,24 @@
 # frozen_string_literal: true
 
+require_relative 'node'
+
 # Builds a binary search tree
 class Tree
   def initialize
     @root = nil
+    @treated_data = false
   end
+
+  def build_tree(data)
+    treated_data = data.sort.uniq
+    mid = treated_data.length / 2
+    node = Node.new(treated_data[mid])
+    return nil if treated_data.empty?
+
+    node.left = build_tree(treated_data[0, mid])
+    node.right = build_tree(treated_data[mid + 1, treated_data.length])
+    @root = node
+  end
+
+  
 end
