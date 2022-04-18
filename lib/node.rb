84bb3390a8d13b7751921ca6a0c3cc48089ba855
Ricardo <ricardomg92@hotmail.com>
2022-04-18 07:14:11 +0200

    Include Comparable module that compares the value of self with the of the provided node

--- lib/node.rb
+++ lib/node.rb
@@ -2,9 +2,16 @@
 
 # Node contains everything necessary for a creation of a new node
 class Node
+  include Comparable
+  attr_accessor :data
+
   def initialize(data, left = nil, right = nil)
     @data = data
     @left = left
     @right = right
+  end
+
+  def <=>(other)
+    @data <=> other.data
   end
 end
