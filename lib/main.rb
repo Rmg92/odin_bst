05c06a57bf7e961d0f04667cac6ff83fdea4a365
Ricardo <ricardomg92@hotmail.com>
2022-04-20 21:59:19 +0200

    Add tests for delete method when deleting leaf node

--- lib/main.rb
+++ lib/main.rb
@@ -3,12 +3,27 @@
 require_relative 'tree'
 
 tree = Tree.new
-tree.build_tree(Array.new(rand(1..15)) { rand(1..100) })
+# tree.build_tree(Array.new(rand(1..15)) { rand(1..100) })
+# puts ''
+# tree.pretty_print
+# puts ''
+
+# tree.insert(rand(1..100))
+# puts ''
+# tree.pretty_print
+# puts ''
+
+tree.build_tree([1, 2, 3, 4])
 puts ''
 tree.pretty_print
 puts ''
 
-tree.insert(rand(1..100))
+tree.delete(1)
+puts ''
+tree.pretty_print
+puts ''
+
+tree.delete(3)
 puts ''
 tree.pretty_print
 puts ''
