095875d9be22e780dc08aeb0be5f0620677df05e
Ricardo <ricardomg92@hotmail.com>
2022-04-23 12:28:40 +0200

    Add test for delete method

--- lib/main.rb
+++ lib/main.rb
@@ -3,35 +3,22 @@
 require_relative 'tree'
 
 tree = Tree.new
-# tree.build_tree(Array.new(rand(1..15)) { rand(1..100) })
-# puts ''
-# tree.pretty_print
-# puts ''
-
-# tree.insert(rand(1..100))
-# puts ''
-# tree.pretty_print
-# puts ''
-
-tree.build_tree([1, 2, 3, 4, 5, 6])
+tree.build_tree(Array.new(rand(1..15)) { rand(1..100) })
 puts ''
 tree.pretty_print
 puts ''
 
-puts 'Delete 4'
-tree.delete(4)
+random_number = rand(1..100)
+puts "Insert #{random_number}"
+tree.insert(random_number)
 puts ''
 tree.pretty_print
 puts ''
 
-puts 'Delete 2'
-tree.delete(2)
-puts ''
-tree.pretty_print
-puts ''
+random_number = rand(1..100)
+puts "Delete #{random_number}"
+tree.delete(random_number)
 
-puts 'Delete 1'
-tree.delete(1)
 puts ''
 tree.pretty_print
 puts ''
